366e1c835a3179d33c6ac772a17f8c5051f231b2
thanhphucnguyen77 <thanhphuc.nguyen@citynow.vn>
2019-01-24 18:09:28 +0700

    new title

--- src/containers/home.js
+++ src/containers/home.js
@@ -21,6 +21,49 @@ const charPoses = {
     }
   };
 
+  $(document).ready(function(){
+    var theLetters = "abcdefghijklmnopqrstuvwxyz#%&^+=-"; //You can customize what letters it will cycle through
+    var ctnt = "SKI LANKA'S FIRST<br/>CRYPTOCURRENCY TRADING FLATFROM"; // Your text goes here
+    var speed = 50; // ms per frame
+    var increment = 8; // frames per step. Must be >2
+    
+        
+    var clen = ctnt.length;       
+    var si = 0;
+    var stri = 0;
+    var block = "";
+    var fixed = "";
+    //Call self x times, whole function wrapped in setTimeout
+    (function rustle (i) {          
+    setTimeout(function () {
+      if (--i){rustle(i);}
+      nextFrame(i);
+      si = si + 1;        
+    }, speed);
+    })(clen*increment+1); 
+    function nextFrame(pos){
+      for (var i=0; i<clen-stri; i++) {
+        //Random number
+        var num = Math.floor(theLetters.length * Math.random());
+        //Get random letter
+        var letter = theLetters.charAt(num);
+        block = block + letter;
+      }
+      if (si == (increment-1)){
+        stri++;
+      }
+      if (si == increment){
+      // Add a letter; 
+      // every speed*10 ms
+      fixed = fixed +  ctnt.charAt(stri - 1);
+      si = 0;
+      }
+      $("#output").html(fixed + block);
+      block = "";
+    }
+    });
+
+
 
 
 
@@ -42,9 +85,9 @@ class HomePage extends React.Component {
         }, 2000)
     }
 
-    componentDidMount() {
-        this.displayMenu();
-    }
+    // componentDidMount() {
+    //     this.displayMenu();
+    // }
 
     render() {
     return (
@@ -53,14 +96,15 @@ class HomePage extends React.Component {
                 this.state.isVisibleTitle ? 
                 <div className="container">
                     <div className="homepage">
-                        <h1>
+                        {/* <h1>
                             <SplitText initialPose="exit" pose="enter" charPoses={charPoses}>
                                 SKI LANKA'S FIRST
                             </SplitText><br/>
                             <SplitText initialPose="exit" pose="enter" charPoses={charPoses}>
                                 CRYPTOCURRENCY TRADING FLATFORM
                             </SplitText>
-                        </h1>
+                        </h1> */}
+                        <div id="output"></div>
                     </div>
                 </div>
                 : ""
