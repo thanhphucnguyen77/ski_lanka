0b4e435463424fc8f53780fc9d7337da61e6b6b4
thanhphucnguyen77 <thanhphuc.nguyen@citynow.vn>
2019-02-11 19:17:28 +0700

    fix footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,14 +6,14 @@ const Footer = (props) => {
     return (
         <div className="footer-container">
             <div className="footer-link">
-                <Row>
-                    <Col span={3}><a>Term of use</a></Col>
-                    <Col span={4}><a> AML&amp;KYC Policy</a></Col>
-                    <Col span={3}><a>  Risk Disclosure </a></Col>
-                    <Col span={4}><a>Conflicts of interest </a></Col>
-                    <Col span={4}><a> Privacy Policy</a></Col>
-                    <Col span={6}><a>Order Excecution Policy</a></Col>
-                </Row>
+                <ul>
+                    <li><a>Term of use</a></li>
+                    <li><a> AML&amp;KYC Policy</a></li>
+                    <li><a>  Risk Disclosure </a></li>
+                    <li><a>Conflicts of interest </a></li>
+                    <li><a> Privacy Policy</a></li>
+                    <li><a>Order Excecution Policy</a></li>
+                </ul>
             </div>
         </div>
     )
